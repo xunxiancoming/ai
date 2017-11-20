b45fa015d0b07297a368edb14ae046441c542dfb
JingxiongZhu <jingxiongzhu@outlook.com>
2017-11-20 13:41:24 +0800

    init

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -17,7 +17,7 @@
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
+import 'qiniu-js'
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
--- resources/views/write.blade.php
+++ resources/views/write.blade.php
@@ -27,7 +27,7 @@
                 form.append('file', upload_file.files[0])
 
                 window.axios.post('http://up-z2.qiniu.com', form).then(res => {
-                        if (res.status === 200) {
+                    if (res.status === 200) {
                         img.src = 'http://ozoz93ox4.bkt.clouddn.com/' + res.data.key
 
                         upload_file.value = null
@@ -82,15 +82,16 @@
                 'redo'  // 重复
             ]
             {{--editor.customConfig.showLinkImg = false // 隐藏“网络图片”tab--}}
+                editor.customConfig.uploadImgServer = 'http://up-z2.qiniu.com'  // 上传图片到服务器
             {{--editor.customConfig.uploadImgServer = '{{ route('api.upload') }}'  // 上传图片到服务器--}}
-            {{--editor.customConfig.uploadImgHeaders = {--}}
-                {{--'X-CSRF-TOKEN': '{{ csrf_token() }}'--}}
-            {{--}--}}
-            {{--editor.customConfig.uploadImgParams = {--}}
-                {{--api_token: '{{ Auth::user()->api_token }}'--}}
-            {{--}--}}
-            {{--editor.customConfig.uploadFileName = 'upload_file'--}}
-            editor.customConfig.debug = true
+                    {{--editor.customConfig.uploadImgHeaders = {--}}
+                    {{--'X-CSRF-TOKEN': '{{ csrf_token() }}'--}}
+                    {{--}--}}
+                    {{--editor.customConfig.uploadImgParams = {--}}
+                    {{--api_token: '{{ Auth::user()->api_token }}'--}}
+                    {{--}--}}
+                    {{--editor.customConfig.uploadFileName = 'upload_file'--}}
+                editor.customConfig.debug = true
             editor.customConfig.qiniu = true
             editor.create()
 
@@ -100,23 +101,23 @@
             // 初始化七牛上传的方法
             function uploadInit() {
                 // 获取相关 DOM 节点的 ID
-                var btnId = editor.imgMenuId;
-                var containerId = editor.toolbarElemId;
-                var textElemId = editor.textElemId;
+                let btnId = editor.imgMenuId
+                let containerId = editor.toolbarElemId
+                let textElemId = editor.textElemId
 
                 // 创建上传对象
-                var uploader = Qiniu.uploader({
+                let uploader = Qiniu.uploader({
                     runtimes: 'html5,flash,html4',    //上传模式,依次退化
                     browse_button: btnId,       //上传选择的点选按钮，**必需**
-                    uptoken_url: '/uptoken',
+                    // uptoken_url: '/uptoken',
                     //Ajax请求upToken的Url，**强烈建议设置**（服务端提供）
-                    // uptoken : '<Your upload token>',
+                    uptoken: '{{ app('util')->getUploadToken() }}',
                     //若未指定uptoken_url,则必须指定 uptoken ,uptoken由其他程序生成
                     // unique_names: true,
                     // 默认 false，key为文件名。若开启该选项，SDK会为每个文件自动生成key（文件名）
                     // save_key: true,
                     // 默认 false。若在服务端生成uptoken的上传策略中指定了 `sava_key`，则开启，SDK在前端将不对key进行任何处理
-                    domain: 'http://7xrjl5.com1.z0.glb.clouddn.com/',
+                    domain: 'http://ozoz93ox4.bkt.clouddn.com/',
                     //bucket 域名，下载资源时用到，**必需**
                     container: containerId,           //上传区域DOM ID，默认是browser_button的父元素，
                     max_file_size: '4mb',             //最大文件体积限制
@@ -124,7 +125,7 @@
                     filters: {
                         mime_types: [
                             //只允许上传图片文件 （注意，extensions中，逗号后面不要加空格）
-                            { title: "图片文件", extensions: "jpg,gif,png,bmp" }
+                            {title: '图片文件', extensions: 'jpg,gif,png,bmp'}
                         ]
                     },
                     max_retries: 3,                   //上传失败最大重试次数
@@ -133,46 +134,40 @@
                     chunk_size: '4mb',                //分块上传时，每片的体积
                     auto_start: true,                 //选择文件后自动上传，若关闭需要自己绑定事件触发上传
                     init: {
-                        'FilesAdded': function(up, files) {
-                            plupload.each(files, function(file) {
-                                // 文件添加进队列后,处理相关的事情
-                                printLog('on FilesAdded');
-                            });
+                        'FilesAdded': (up, files) => {
+                            plupload.each(files, function (file) {
+                                printLog('on FilesAdded')
+                            })
                         },
-                        'BeforeUpload': function(up, file) {
-                            // 每个文件上传前,处理相关的事情
-                            printLog('on BeforeUpload');
+                        'BeforeUpload': (up, file) => {
+                            printLog('on BeforeUpload')
                         },
-                        'UploadProgress': function(up, file) {
-                            // 显示进度
+                        'UploadProgress': (up, file) => {
                             printLog('进度 ' + file.percent)
                         },
-                        'FileUploaded': function(up, file, info) {
+                        'FileUploaded': (up, file, info) => {
                             // 每个文件上传成功后,处理相关的事情
                             // 其中 info 是文件上传成功后，服务端返回的json，形式如
                             // {
                             //    "hash": "Fh8xVqod2MQ1mocfI4S4KpRL6D98",
                             //    "key": "gogopher.jpg"
                             //  }
-                            printLog(info);
-                            // 参考http://developer.qiniu.com/docs/v6/api/overview/up/response/simple-response.html
+                            printLog(info)
 
-                            var domain = up.getOption('domain');
-                            var res = $.parseJSON(info);
-                            var sourceLink = domain + res.key; //获取上传成功后的文件的Url
+                            let domain = up.getOption('domain')
+                            let res = JSON.parse(info)
+                            let sourceLink = domain + res.key //获取上传成功后的文件的Url
 
-                            printLog(sourceLink);
+                            printLog(sourceLink)
 
-                            // 插入图片到editor
                             editor.cmd.do('insertHtml', '<img src="' + sourceLink + '" style="max-width:100%;"/>')
                         },
-                        'Error': function(up, err, errTip) {
-                            //上传出错时,处理相关的事情
-                            printLog('on Error');
+                        'Error': (up, err, errTip) => {
+                            printLog('on Error')
                         },
-                        'UploadComplete': function() {
+                        'UploadComplete': () => {
                             //队列文件处理完毕后,处理相关的事情
-                            printLog('on UploadComplete');
+                            printLog('on UploadComplete')
                         }
                         // Key 函数如果有需要自行配置，无特殊需要请注释
                         //,
@@ -184,14 +179,12 @@
                         //     return key
                         // }
                     }
-                    // domain 为七牛空间（bucket)对应的域名，选择某个空间后，可通过"空间设置->基本设置->域名设置"查看获取
-                    // uploader 为一个plupload对象，继承了所有plupload的方法，参考http://plupload.com/docs
-                });
+                })
             }
 
             // 封装 console.log 函数
             function printLog(title, info) {
-                window.console && console.log(title, info);
+                window.console && console.log(title, info)
             }
 
             // Submit
